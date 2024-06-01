8d0d8957c16e82b139652bdda67e6e2ad9f4a934
Tero Keränen <tero-keranen@hotmail.com>
2024-06-01 10:44:58 +0300

    debugging front page

--- src/screens/Etusivu.js
+++ src/screens/Etusivu.js
@@ -19,201 +19,207 @@ import WorkOn from "../components/EtusivuComponents/WorkOn";
 
 
 const Etusivu = ({navigation}) => {
-  const { t } = useTranslation(); //Säilytä 
-  const { state, fetchUser } = useContext(Autcontext); 
-  const {state: eventState,fetchEvents } = useContext(EventContext) 
-  const {state: worksiteState,fetchWorksites} = useContext(WorksiteContext)
-  const [events, setEvents] = useState([]); 
-  const [isLoading, setIsLoading] = useState(false);
-  const [selectedTitle, setSelectedTitle] = useState(null); // Käytetään tätä kun valitaan mitä halutaan näkyväksi
-  
-  const role = state?.user?.role;
-  const userId = state?.user?._id;
-  const worksites = worksiteState.worksites;
-  const company = state.user?.company;
-  
-  
-  
-  
-  
-  
-  
-  
-  const handlePress = (title) => {
-    if (selectedTitle === title) {
-      setSelectedTitle(null);
-    } else {
-      setSelectedTitle(title);
-      
-    }
-  }
-  
-  
-  
-  // Haetaan AuthCOntect.js avulla tiedot käyttäjästä.
-  
-  
-  useEffect(() => {
-    const fetchAndSetData =  async () => {
-      setIsLoading(true);
-       await fetchUser();
-
-       if (company != null) {
-          console.log("TOTEUTETAAN TÄMÄ")
-         await fetchWorksites();
-       }
-       await fetchEvents();
-      setIsLoading(false)
-      
-    }
-    const unsubscribe = navigation.addListener('focus', fetchAndSetData)
-
-
-    return unsubscribe;
-  })
-
-  useEffect(() => {
-    
-      
-        fetchWorksites();
-      
-    
-  }, []);
-  
-
-  
-  
-
-  useEffect(() => {
-    if (eventState.events) {
-      // Käännetään lista siten että ekana näkyy uusimmat
-      const reservedEvents = [...eventState.events].reverse();
-      setEvents(reservedEvents);
-    }
-  }, [eventState.events])
-
-
-  let worksitesToShow = worksiteState.worksites;
-
-  
-  
-  if (role === "user") {
-    worksitesToShow = worksiteState.worksites.filter(worksite => 
-      worksite.workers.includes(userId)
-      )
-  }
-  let futureStart = []; // luodaan muuttuja johon laitetaan tulevaisuudessa alkavat työmaat
-  let notReadyWorksites = []; // luodaan muuttuja johon laitetaan työmaat jotka on aloitettu mutta ei ole valmiina
-  
-  worksitesToShow.forEach(worksite => {
-    if (!worksite) {
-      console.log("skipping undefined worksite")
-      return;
-    }
-    
-    if (!worksite.workDays || worksite.workDays.length === 0) {
-      // Jos workDays on tyhjä, tarkista onko työmaan aloitusaika tulevaisuudessa
-      if (futureStartTime(worksite.startTime)) {
-        futureStart.push(worksite);
-      }
-    } else if (!worksite.isReady) {
-      // Jos workDays ei ole tyhjä, mutta työmaa ei ole valmis, lisää notReadyWorksites-listaan
-      notReadyWorksites.push(worksite);
-    }
-  });
-  const readyWorksites = worksitesToShow.filter(worksite => worksite.isReady === true);
-  
-
-
-  const accordionData = [
-    { title: t('etusivuEventsButton'), content: <Events events={events} /> },
-    { title: t('etusivuReadyButton'), content: <WorksiteReady worksites={readyWorksites} title={t('ready')} /> },
-    { title: t('etusivuUnfinishedButton'), content: <WorksiteReady worksites={notReadyWorksites} title={t('unfinished')} /> },
-    { title: t('etusivuStartingdButton'), content: <WorksiteReady worksites={futureStart} title={t('starting')} /> },
-    { title: "Ongoing", content: <WorkOn worksites={worksites} userRole={role} userId={userId}/> },
-    // Lisää muita otsikoita ja sisältöjä tarpeen mukaan
-  ];
-
-
-  const accordionRows = [];
-  for (let i = 0; i < accordionData.length; i += 3) {
-    accordionRows.push(accordionData.slice(i, i + 3));
-  }
-
-
-
-
-  if (isLoading) {
-    return <DownloadScreen message={t('loading')} />
-  }
 
   return (
-      
-        <ImageBackground
-          source={require('../../assets/logo-color.png')}
-          
-          style={styles.background}
-        >
-        <View style={styles.overlay}>
-
-        <View style={styles.container}>
-          
-        {accordionRows.map((row, rowIndex) => (
-            <View style={styles.accContainer} key={rowIndex}>
-              {row.map((item, index) => (
-                <Accordion
-                  key={index}
-                  title={item.title}
-                  handlePress={() => handlePress(item.title)}
-                  isSelected={selectedTitle === item.title}
-                />
-              ))}
-            </View>
-          ))}
-          
-          
-          {selectedTitle ? (
-            accordionData.map((item, index) => (
-              selectedTitle === item.title && <View style={styles.test} key={index}>{item.content}</View>
-            ))
-          ) : (
-            null
-          )}
-          
-
-        </View>
-      
-          </View>
-          </ImageBackground>
-    
+    <View>
+      <Text>Moi</Text>
+    </View>
   )
+  // const { t } = useTranslation(); //Säilytä 
+  // const { state, fetchUser } = useContext(Autcontext); 
+  // const {state: eventState,fetchEvents } = useContext(EventContext) 
+  // const {state: worksiteState,fetchWorksites} = useContext(WorksiteContext)
+  // const [events, setEvents] = useState([]); 
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [selectedTitle, setSelectedTitle] = useState(null); // Käytetään tätä kun valitaan mitä halutaan näkyväksi
+  
+  // const role = state?.user?.role;
+  // const userId = state?.user?._id;
+  // const worksites = worksiteState.worksites;
+  // const company = state.user?.company;
+  
+  
+  
+  
+  
+  
+  
+  
+  // const handlePress = (title) => {
+  //   if (selectedTitle === title) {
+  //     setSelectedTitle(null);
+  //   } else {
+  //     setSelectedTitle(title);
+      
+  //   }
+  // }
+  
+  
+  
+  // // Haetaan AuthCOntect.js avulla tiedot käyttäjästä.
+  
+  
+  // useEffect(() => {
+  //   const fetchAndSetData =  async () => {
+  //     setIsLoading(true);
+  //      await fetchUser();
+
+  //      if (company != null) {
+  //         console.log("TOTEUTETAAN TÄMÄ")
+  //        await fetchWorksites();
+  //      }
+  //      await fetchEvents();
+  //     setIsLoading(false)
+      
+  //   }
+  //   const unsubscribe = navigation.addListener('focus', fetchAndSetData)
+
+
+  //   return unsubscribe;
+  // })
+
+  // useEffect(() => {
+    
+      
+  //       fetchWorksites();
+      
+    
+  // }, []);
+  
+
+  
+  
+
+  // useEffect(() => {
+  //   if (eventState.events) {
+  //     // Käännetään lista siten että ekana näkyy uusimmat
+  //     const reservedEvents = [...eventState.events].reverse();
+  //     setEvents(reservedEvents);
+  //   }
+  // }, [eventState.events])
+
+
+  // let worksitesToShow = worksiteState.worksites;
+
+  
+  
+  // if (role === "user") {
+  //   worksitesToShow = worksiteState.worksites.filter(worksite => 
+  //     worksite.workers.includes(userId)
+  //     )
+  // }
+  // let futureStart = []; // luodaan muuttuja johon laitetaan tulevaisuudessa alkavat työmaat
+  // let notReadyWorksites = []; // luodaan muuttuja johon laitetaan työmaat jotka on aloitettu mutta ei ole valmiina
+  
+  // worksitesToShow.forEach(worksite => {
+  //   if (!worksite) {
+  //     console.log("skipping undefined worksite")
+  //     return;
+  //   }
+    
+  //   if (!worksite.workDays || worksite.workDays.length === 0) {
+  //     // Jos workDays on tyhjä, tarkista onko työmaan aloitusaika tulevaisuudessa
+  //     if (futureStartTime(worksite.startTime)) {
+  //       futureStart.push(worksite);
+  //     }
+  //   } else if (!worksite.isReady) {
+  //     // Jos workDays ei ole tyhjä, mutta työmaa ei ole valmis, lisää notReadyWorksites-listaan
+  //     notReadyWorksites.push(worksite);
+  //   }
+  // });
+  // const readyWorksites = worksitesToShow.filter(worksite => worksite.isReady === true);
+  
+
+
+  // const accordionData = [
+  //   { title: t('etusivuEventsButton'), content: <Events events={events} /> },
+  //   { title: t('etusivuReadyButton'), content: <WorksiteReady worksites={readyWorksites} title={t('ready')} /> },
+  //   { title: t('etusivuUnfinishedButton'), content: <WorksiteReady worksites={notReadyWorksites} title={t('unfinished')} /> },
+  //   { title: t('etusivuStartingdButton'), content: <WorksiteReady worksites={futureStart} title={t('starting')} /> },
+  //   { title: "Ongoing", content: <WorkOn worksites={worksites} userRole={role} userId={userId}/> },
+  //   // Lisää muita otsikoita ja sisältöjä tarpeen mukaan
+  // ];
+
+
+  // const accordionRows = [];
+  // for (let i = 0; i < accordionData.length; i += 3) {
+  //   accordionRows.push(accordionData.slice(i, i + 3));
+  // }
+
+
+
+
+  // if (isLoading) {
+  //   return <DownloadScreen message={t('loading')} />
+  // }
+
+  // return (
+      
+  //       <ImageBackground
+  //         source={require('../../assets/logo-color.png')}
+          
+  //         style={styles.background}
+  //       >
+  //       <View style={styles.overlay}>
+
+  //       <View style={styles.container}>
+          
+  //       {accordionRows.map((row, rowIndex) => (
+  //           <View style={styles.accContainer} key={rowIndex}>
+  //             {row.map((item, index) => (
+  //               <Accordion
+  //                 key={index}
+  //                 title={item.title}
+  //                 handlePress={() => handlePress(item.title)}
+  //                 isSelected={selectedTitle === item.title}
+  //               />
+  //             ))}
+  //           </View>
+  //         ))}
+          
+          
+  //         {selectedTitle ? (
+  //           accordionData.map((item, index) => (
+  //             selectedTitle === item.title && <View style={styles.test} key={index}>{item.content}</View>
+  //           ))
+  //         ) : (
+  //           null
+  //         )}
+          
+
+  //       </View>
+      
+  //         </View>
+  //         </ImageBackground>
+    
+  // )
 
 };
 
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    height: '100%',
-  },
-  test: {
-    alignItems: 'center'
-  },
-  accContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-evenly',
-    // Lisää tähän tarvittavat tyylit
-  },
-  background: {
-    flex: 1, // Varmista, että ImageBackground täyttää koko näytön
-    width: '100%',
-    height: '100%',
-  },
-  overlay: {
-    flex: 1,
-    backgroundColor: 'rgba(40, 42, 54, 0.1)',  // Määritä väri ja läpinäkyvyys tarpeen mukaan
-  },
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     height: '100%',
+//   },
+//   test: {
+//     alignItems: 'center'
+//   },
+//   accContainer: {
+//     flexDirection: 'row',
+//     justifyContent: 'space-evenly',
+//     // Lisää tähän tarvittavat tyylit
+//   },
+//   background: {
+//     flex: 1, // Varmista, että ImageBackground täyttää koko näytön
+//     width: '100%',
+//     height: '100%',
+//   },
+//   overlay: {
+//     flex: 1,
+//     backgroundColor: 'rgba(40, 42, 54, 0.1)',  // Määritä väri ja läpinäkyvyys tarpeen mukaan
+//   },
   
-});
+// });
 
 export default Etusivu;
